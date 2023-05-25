294194e3b9dfd834f1d4c43c02e5882ade9c395c
Nastasia <lisitsinan@gmail.com>
2023-05-25 23:34:26 +0300

    fix: sort algorithms, boundary conditions

--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -22,38 +22,38 @@ export const selectionSort = (arr: number[], direction: Direction) => {
     indexJ: null,
     sortedRange: [],
   });
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let minIndex = i;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-
-    for (let j = i + 1; j < arr.length; j++) {
-      steps.push({
-        currentArray: [...arr],
-        indexI: i,
-        indexJ: j,
-        sortedRange: [...steps[steps.length - 1]?.sortedRange],
-      });
-      if (
-        direction === Direction.Ascending
-          ? arr[j] < arr[minIndex]
-          : arr[j] > arr[minIndex]
-      ) {
-        minIndex = j;
+      for (let j = i + 1; j < arr.length; j++) {
+        steps.push({
+          currentArray: [...arr],
+          indexI: i,
+          indexJ: j,
+          sortedRange: [...steps[steps.length - 1]?.sortedRange],
+        });
+        if (
+          direction === Direction.Ascending
+            ? arr[j] < arr[minIndex]
+            : arr[j] > arr[minIndex]
+        ) {
+          minIndex = j;
+        }
       }
+      if (minIndex !== i) {
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      }
+      steps[steps.length - 1].sortedRange.push(i);
     }
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
-    steps[steps.length - 1].sortedRange.push(i);
+
+    steps.push({
+      currentArray: [...arr],
+      indexI: null,
+      indexJ: null,
+      sortedRange: [...steps[steps.length - 1].sortedRange, arr.length - 1],
+    });
   }
-
-  steps.push({
-    currentArray: [...arr],
-    indexI: null,
-    indexJ: null,
-    sortedRange: [...steps[steps.length - 1].sortedRange, arr.length - 1],
-  });
-
   return steps;
 };
 
@@ -66,46 +66,53 @@ export const bubbleSort = (arr: number[], direction: Direction) => {
     indexJ: null,
     sortedRange: [],
   });
-  let isSorted = false;
-  for (let i = 0; i < arr.length; i++) {
-    if (isSorted) {
-      
-      steps.push({
-        currentArray: [...arr],
-        indexI: null,
-        indexJ: null,
-        sortedRange: [...steps[steps.length - 1].sortedRange],
-      });
-      for (let h = 0; h < arr.length - i; h++) {
-        steps[steps.length - 1].sortedRange.push(h);
+  if (arr.length > 0) {
+    let isSorted = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (isSorted) {
+        steps.push({
+          currentArray: [...arr],
+          indexI: null,
+          indexJ: null,
+          sortedRange: [...steps[steps.length - 1].sortedRange],
+        });
+        for (let h = 0; h < arr.length - i; h++) {
+          steps[steps.length - 1].sortedRange.push(h);
+        }
+        return steps;
       }
-      return steps;
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        steps.push({
+          currentArray: [...arr],
+          indexI: j,
+          indexJ: j + 1,
+          sortedRange: [...steps[steps.length - 1]?.sortedRange],
+        });
+        isSorted = true;
+        if (
+          direction === Direction.Ascending
+            ? arr[j] > arr[j + 1]
+            : arr[j] < arr[j + 1]
+        ) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          isSorted = false;
+        }
+        if (j+1 === arr.length - i - 1) {
+          steps[steps.length - 1].sortedRange.push(arr.length - i - 1);
+        }
+      }
     }
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      steps.push({
-        currentArray: [...arr],
-        indexI: j,
-        indexJ: j+1,
-        sortedRange: [...steps[steps.length - 1]?.sortedRange],
-      });
-      isSorted = true;
-      if (
-        direction === Direction.Ascending
-          ? arr[j] > arr[j + 1]
-          : arr[j] < arr[j + 1]
-      ) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        isSorted = false;
-      } 
-    }
-    steps[steps.length - 1].sortedRange.push(arr.length - i - 1);
-  }
 
-  steps.push({
-    currentArray: [...arr],
-    indexI: null,
-    indexJ: null,
-    sortedRange: [...steps[steps.length - 1].sortedRange, arr.length - 1],
-  });
+    steps.push({
+      currentArray: [...arr],
+      indexI: null,
+      indexJ: null,
+      sortedRange: [...steps[steps.length - 1].sortedRange],
+    });
+
+    if (steps[steps.length - 1].sortedRange.length !== arr.length) {
+      steps[steps.length - 1].sortedRange.push(0);
+    }
+  }
   return steps;
 };
